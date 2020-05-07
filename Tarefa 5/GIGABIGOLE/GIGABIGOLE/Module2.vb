34dc2ce928cae490b349f536f3cc1529e516f9bb
Nikk0PT <61123353+Nikk0PT@users.noreply.github.com>
2020-05-07 11:03:12 +0100

    Add files via upload

---
+++ Tarefa 5/GIGABIGOLE/GIGABIGOLE/Module2.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Module Module2
+
+    Sub main()
+        Dim f As FileStream
+
+        Try
+
+            f = New FileStream("alfabeto.dat", FileMode.Open)
+        Catch ex As Exception
+
+            Console.WriteLine("Erro ao abrir o ficheiro!" & vbNewLine & ex.Message)
+            Return
+        End Try
+        Dim ch As Char
+        Try
+            For i As Integer = 0 To CInt(f.Length - 1)
+                f.Seek(i, SeekOrigin.Begin)
+                ch = ChrW(f.ReadByte)
+                Console.Write(ch)
+            Next
+        Catch ex As Exception
+
+            Console.WriteLine("Erro ao ler o ficheiro." & vbNewLine & ex.Message)
+        End Try
+        f.close()
+
+    End Sub
+End Module
---
+++ Tarefa 5/PimbaDoGigabigole/PimbaDoGigabigole/Module1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+Module Module1
+
+    Sub Main()
+        Console.Write("Login:> ")
+        Dim strLogin As String = Console.ReadLine
+        Console.Write("Senha:> ")
+        Dim strSenha As String = Console.ReadLine
+        If Len(strLogin) > 0 And Len(strSenha) > 0 Then
+            Try
+                Dim bw As New StreamWriter(New FileStream("user.txt", FileMode.Create))
+                Try
+                    bw.Write(strLogin)
+                    bw.Write(";")
+                    bw.Write(strSenha)
+                    bw.Write(";")
+                Catch ex As Exception
+
+                    Console.WriteLine("Erro na escrita. " & vbNewLine & ex.Message)
+
+                End Try
+                bw.Close()
+            Catch ex As Exception
+
+                Console.WriteLine("Erro ao criar o ficheiro!" & vbNewLine & ex.Message)
+                Return
+
+            End Try
+        End If
+        Console.Read()
+    End Sub
+
+End Module
